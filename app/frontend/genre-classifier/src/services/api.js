fea8397284a07ee732d149c9a51b3caf3f5cd22c
ferro <ferit.sulce@student.howest.be>
2025-06-11 16:36:09 +0200

    fixed nginx.conf

--- app/frontend/genre-classifier/src/services/api.js
+++ app/frontend/genre-classifier/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const apiClient = axios.create({
-  baseURL: '/home/api',
+  baseURL: '/api',
   withCredentials: false,
   headers: {
     'Accept': 'application/json',
